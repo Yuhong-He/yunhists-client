056f2bac779e44ce840e18a72b916a41cd747d4c
Yuhong He <71640320+Theodore-Ho@users.noreply.github.com>
2023-03-21 22:11:44 +0000

    some update

--- src/store/index.js
+++ src/store/index.js
@@ -23,7 +23,7 @@ export default new Vuex.Store({
   },
   plugins: [
     createPersistedstate({
-      key: 'User',
+      key: 'Yunhists',
       paths: ['Settings', 'User', 'Aliyun']
     })
   ]
